e3c7488f51fe83be8ad9d31255b86e3e172d3f6c
Sprattagg <dubois03330@gmail.com>
2020-02-27 16:30:14 +0100

    Update loginform.php

--- views/loginform.php
+++ views/loginform.php
@@ -1,6 +1,6 @@
 <form method="POST" action="login.php">
             <input type="text" name="user_name" placeholder="username" class="form_username" required>
             <input type="password" name="password" placeholder="password" required><br>
-            <input class="login" type="submit" value="Log in">
+            <input class="login" type="submit" value="Log inn">
         </form>
 
