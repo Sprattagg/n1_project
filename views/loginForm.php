5c5dc649bfb0ff060717f6f3a3b2ea679587d91d
bjorngrape <56294098+bjorngrape@users.noreply.github.com>
2020-02-27 16:37:22 +0100

    Create loginForm.php

---
+++ views/loginForm.php
@@ -0,0 +1,5 @@
+<form method="POST" action="login.php">
+            <input type="text" name="user_name" placeholder="username" class="form_username" required>
+            <input type="password" name="password" placeholder="password" required><br>
+            <input class="login" type="submit" value="Log in">
+        </form>
